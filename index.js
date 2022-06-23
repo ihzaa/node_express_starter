706213d7ceeb10294b0423433e4c7b13dda978b1
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-06-23 13:48:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// ADD ROUTE START
+const fs = require("fs");
+const routePath = "./routes";
+const files = fs.readdirSync(routePath);
+
+files.forEach((file) => {
+  let name = file.split(".")[0];
+  const importedRoute = require(`${routePath}/${name}`);
+  app.use(`/${name}`, importedRoute);
+});
+// ADD ROUTE END
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running in port: " + process.env.PORT);
+});
