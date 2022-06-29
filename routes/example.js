ceb958d15a64c40f8d1d1fc5eb5f06fa3af7b310
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2022-06-29 11:34:31 +0700

    add comment in example route

--- routes/example.js
+++ routes/example.js
@@ -1,5 +1,11 @@
 // just add some file like this for add route
 // you can create a folder, folder name must be unique in a root
+// route path is consider of the foler name and file name. Example:
+// there is a route call auth.js inside routes/api/
+// + routes
+//   + api
+//     - auth.js
+// so the route path is IP:PORT/api/auth/xxxx
 
 const express = require("express");
 const router = express.Router();
